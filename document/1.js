7be4b6b5273f0ed1e76c14cb7b58fa289e706553
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2022-08-17 02:58:09 +0900

    Solve issue 1 Fixes #1

--- document/1.js
+++ document/1.js
@@ -19,7 +19,9 @@ sayHi();
 
 // varとletで変数宣言する前にconsole.log が走るのがポイント
 // 宣言する前にはどういう状態になっているのかを知っているか
-    // 
+    // var: undefinedとしてメモリが確保される
+    // let: メモリの確保がない。これは"temporal dead zone"と呼ばれる。constも同じ
 // その状態ではどのような値やエラーとして処理されるのか
-    //
-// 結果
+    //undefined: undefinedが結果として出力される
+    //なにも値が設定されていない場合は、　reference errorとなる
+// 回答： D
