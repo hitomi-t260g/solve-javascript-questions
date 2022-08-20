5743e9731fd533d669c183c33711bd1accaa6183
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2022-08-21 01:39:40 +0900

    1.jsに追記し、フォルダ分けを変更

--- answer/1.js
+++ answer/1.js
@@ -1,6 +1,5 @@
 // 1. 何が出力されるでしょうか？
 
-
 function sayHi() {
   console.log(name);
   console.log(age);
@@ -10,16 +9,25 @@ function sayHi() {
 
 sayHi();
 
-
 // A: Lydia と undefined
 // B: Lydia と ReferenceError
 // C: ReferenceError と 21
-// D: undefined と ReferenceError 
-
+// D: undefined と ReferenceError
 
 // varとletで変数宣言する前にconsole.log が走るのがポイント
 // 宣言する前にはどういう状態になっているのかを知っているか
-    // 
+//
 // その状態ではどのような値やエラーとして処理されるのか
-    //
+//
 // 結果
+
+// Lydia と 21を出したかったら、以下の書き方にすればよい
+// 上記選択肢で順番入れ替えだけでは再現できないものはB（varをletとかconstにすればできる）
+// function sayHi() {
+//   var name = "Lydia";
+//   let age = 21;
+//   console.log(name);
+//   console.log(age);
+// }
+
+// sayHi();
